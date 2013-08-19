52539dd48987cc42493a1c2506039aacf9999e02
Techdruid <simoneurbinati@outlook.com>
2013-08-19 20:29:14 +0200

    Initial commit

---
+++ Empat/src/com/simoneurbinati/empat/util/Config.java
@@ -0,0 +1,6 @@
+package com.simoneurbinati.empat.util;
+
+public class Config {
+	public static final String SERVER_DEFAULT_BASE_URL = "http://www.simoneurbinati.com/Empat/";
+	public static final String GCM_SENDER_ID = "619465775980";
+}
---
+++ Empat/src/com/simoneurbinati/empat/util/Utils.java
@@ -0,0 +1,5 @@
+package com.simoneurbinati.empat.util;
+
+public class Utils {
+
+}
