e57fb7b170a8e57e67c6c79d0ffd6139688ff1b6
Techdruid <simoneurbinati@outlook.com>
2013-08-29 16:07:08 +0200

    Messages activity UI

--- Empat/gen/com/simoneurbinati/empat/R.java
+++ Empat/gen/com/simoneurbinati/empat/R.java
@@ -982,6 +982,7 @@ containing a value of this type.
         public static final int message_received=0x7f02005a;
         public static final int message_sent=0x7f02005b;
         public static final int send=0x7f02005c;
+        public static final int send_now=0x7f02005d;
     }
     public static final class id {
         public static final int action_bar=0x7f05001a;
@@ -1003,16 +1004,17 @@ containing a value of this type.
         public static final int button1=0x7f050047;
         public static final int checkbox=0x7f05002b;
         public static final int collapseActionView=0x7f05000d;
-        public static final int compose=0x7f050050;
-        public static final int contacts=0x7f05004e;
+        public static final int compose=0x7f050053;
+        public static final int contacts=0x7f050051;
         public static final int default_activity_button=0x7f050026;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
-        public static final int display_name=0x7f05004a;
+        public static final int display_name=0x7f05004d;
         public static final int dropdown=0x7f05000f;
         public static final int editText1=0x7f050046;
         public static final int edit_query=0x7f050036;
         public static final int end=0x7f050013;
+        public static final int enter_message=0x7f05004b;
         public static final int expand_activities_button=0x7f050024;
         public static final int expanded_menu=0x7f05002a;
         public static final int home=0x7f050014;
@@ -1020,8 +1022,9 @@ containing a value of this type.
         public static final int icon=0x7f050028;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050025;
-        public static final int last_message_timestamp=0x7f05004c;
+        public static final int last_message_timestamp=0x7f05004f;
         public static final int left_icon=0x7f050031;
+        public static final int linearLayout=0x7f05004a;
         public static final int listMode=0x7f050001;
         public static final int listView1=0x7f050044;
         public static final int list_item=0x7f050027;
@@ -1047,7 +1050,8 @@ containing a value of this type.
         public static final int search_plate=0x7f05003c;
         public static final int search_src_text=0x7f05003d;
         public static final int search_voice_btn=0x7f050041;
-        public static final int send=0x7f05004f;
+        public static final int send=0x7f050052;
+        public static final int send_button=0x7f05004c;
         public static final int shortcut=0x7f05002c;
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
@@ -1057,11 +1061,11 @@ containing a value of this type.
         public static final int submit_area=0x7f05003f;
         public static final int tabMode=0x7f050002;
         public static final int textView2=0x7f050045;
-        public static final int timestamp=0x7f05004d;
+        public static final int timestamp=0x7f050050;
         public static final int title=0x7f050029;
         public static final int title_container=0x7f050030;
         public static final int top_action_bar=0x7f05001e;
-        public static final int unread_messages=0x7f05004b;
+        public static final int unread_messages=0x7f05004e;
         public static final int up=0x7f05001f;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
@@ -1120,10 +1124,11 @@ containing a value of this type.
         public static final int activity_conversationlist=0x7f030019;
         public static final int activity_login=0x7f03001a;
         public static final int activity_messages=0x7f03001b;
-        public static final int conversationlist_element=0x7f03001c;
-        public static final int list_element_message_received=0x7f03001d;
-        public static final int list_element_message_sent=0x7f03001e;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001f;
+        public static final int activity_messages_m=0x7f03001c;
+        public static final int conversationlist_element=0x7f03001d;
+        public static final int list_element_message_received=0x7f03001e;
+        public static final int list_element_message_sent=0x7f03001f;
+        public static final int support_simple_spinner_dropdown_item=0x7f030020;
     }
     public static final class menu {
         public static final int activity_compose=0x7f0c0000;
@@ -1182,6 +1187,7 @@ containing a value of this type.
         public static final int hint_message=0x7f0a001a;
         public static final int hint_phone_number=0x7f0a0019;
         public static final int insert_nick=0x7f0a0010;
+        public static final int message_hint=0x7f0a002b;
         public static final int missing_message=0x7f0a0021;
         public static final int missing_phone_number=0x7f0a0020;
         public static final int notification_text=0x7f0a0027;
--- Empat/src/com/simoneurbinati/empat/activities/Messages.java
+++ Empat/src/com/simoneurbinati/empat/activities/Messages.java
@@ -1,15 +1,24 @@
 package com.simoneurbinati.empat.activities;
 
+import java.net.URL;
+
 import com.simoneurbinati.empat.R;
+import com.simoneurbinati.empat.net.Server;
 import com.simoneurbinati.empat.persistence.MessagesContentProvider;
 import com.simoneurbinati.empat.utils.Utility;
 
 import android.annotation.TargetApi;
 import android.app.ActionBar;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
@@ -18,14 +27,20 @@ import android.support.v4.content.Loader;
 import android.support.v4.widget.CursorAdapter;
 import android.support.v7.app.ActionBarActivity;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Messages extends ActionBarActivity implements LoaderManager.LoaderCallbacks<Cursor>{
 
@@ -38,6 +53,10 @@ public class Messages extends ActionBarActivity implements LoaderManager.LoaderC
 	private ListView mListView;
 
 	private CursorAdapter mCursorAdapter;
+	
+	private EditText sendMessage;
+	
+	private Button buttonSend;
 
 	@Override
 	@TargetApi(11)
@@ -83,6 +102,21 @@ public class Messages extends ActionBarActivity implements LoaderManager.LoaderC
 		mListView.setAdapter(mCursorAdapter);
 		// Carica i messaggi con un loader asincrono.
 		getSupportLoaderManager().initLoader(1, null, this);
+		
+		sendMessage = (EditText) findViewById(R.id.enter_message);
+		
+		buttonSend = (Button) findViewById(R.id.send_button);
+
+		buttonSend.setOnClickListener( new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+            	sendMessage();
+                
+            }
+        });
+		
+		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 	}
 
 	@Override
@@ -108,21 +142,21 @@ public class Messages extends ActionBarActivity implements LoaderManager.LoaderC
 //		return false;
 //	}
 	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		int id = item.getItemId();
-		if (id == android.R.id.home) {
-			finish();
-			return true;
-		}
-		if (id == R.id.compose) {
-			Intent intent = new Intent(this, Compose.class);
-			intent.putExtra("phone_number", mPhoneNumber);
-			startActivity(intent);
-			return true;
-		}
-		return false;
-	}
+//	@Override
+//	public boolean onOptionsItemSelected(MenuItem item) {
+//		int id = item.getItemId();
+//		if (id == android.R.id.home) {
+//			finish();
+//			return true;
+//		}
+//		if (id == R.id.compose) {
+//			Intent intent = new Intent(this, Compose.class);
+//			intent.putExtra("phone_number", mPhoneNumber);
+//			startActivity(intent);
+//			return true;
+//		}
+//		return false;
+//	}
 	
 
 	@Override
@@ -177,6 +211,122 @@ public class Messages extends ActionBarActivity implements LoaderManager.LoaderC
 			frame.addView(messageView);
 		}
 	}
+	
+	public void sendMessage() {
+		// Recupera e valida i campi del modulo.
+		String recipientPhoneNumber = mPhoneNumber.trim();
+		if (recipientPhoneNumber.length() == 0) {
+			Toast.makeText(this, getString(R.string.missing_phone_number), Toast.LENGTH_SHORT).show();
+			return;
+		}
+		String message = sendMessage.getText().toString().trim();
+		if (message.length() == 0) {
+			//Toast.makeText(this, getString(R.string.missing_message), Toast.LENGTH_SHORT).show();
+			return;
+		}
+		// Aggiunge il prefisso al numero, se necessario.
+		if (!recipientPhoneNumber.startsWith("+")) {
+			recipientPhoneNumber = "+" + Utility.resolveCurrentICC(this) + recipientPhoneNumber;
+		}
+		// Risolve gli ulteriori dati necessari per l'invio.
+		SharedPreferences prefs = getSharedPreferences("registration", MODE_PRIVATE);
+		String serverBaseUrl = prefs.getString("server_address", null);
+		String privateKey = prefs.getString("private_key", null);
+		String senderPhoneNumber = prefs.getString("phone_number", null);
+		// Avvia un task parallelo per la chiamata al server.
+		AsyncTask<String, Void, Exception> task = new AsyncTask<String, Void, Exception>() {
 
+			private ProgressDialog dialog;
+
+			@Override
+			protected void onPreExecute() {
+				dialog = new ProgressDialog(Messages.this);
+				dialog.setTitle(getString(R.string.send_dialog_title));
+				dialog.setMessage(getString(R.string.send_dialog_message));
+				dialog.setIndeterminate(true);
+				dialog.setCancelable(true);
+				dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+					@Override
+					public void onCancel(DialogInterface dialog) {
+						cancel(true);
+					}
+				});
+				dialog.show();
+			}
+
+			@Override
+			protected Exception doInBackground(String... params) {
+				Exception error = null;
+				try {
+					Server.send(new URL(params[0]), params[1], params[2], params[3], params[4]);
+				} catch (Exception e) {
+					Log.e("freem", "impossibile inviare il messaggio", e);
+					error = e;
+				}
+				if (error == null) {
+					// Salva su database.
+					saveSentMessage(params[3], params[4]);
+				}
+				return error;
+			}
+
+			@Override
+			protected void onPostExecute(Exception error) {
+			    try {
+			        dialog.dismiss();
+			        dialog = null;
+			    } catch (Exception e) {
+			    }
+				if (error == null) {
+					// Termina il composer.
+					finish();
+					// Mostra avviso.
+					Toast.makeText(Messages.this, R.string.send_toast_success, Toast.LENGTH_SHORT).show();
+				} else {
+					// Mostra errore.
+					if (error instanceof Server.UnknownRecipientException) {
+						Toast.makeText(Messages.this, R.string.send_toast_unknown_recipient, Toast.LENGTH_LONG).show();
+					} else {
+						Toast.makeText(Messages.this, R.string.send_toast_error, Toast.LENGTH_SHORT).show();
+					}
+				}
+			}
+
+		};
+		task.execute(serverBaseUrl, privateKey, senderPhoneNumber, recipientPhoneNumber, message);
+	}
+
+	private void saveSentMessage(String phoneNumber, String message) {
+		long now = System.currentTimeMillis();
+		ContentResolver r = getContentResolver();
+		// Cerca se esiste un thread per l'interlocutore.
+		long threadId = -1;
+		Cursor c = r.query(MessagesContentProvider.THREADS_URI, new String[] { "_id" }, "phone_number = ?", new String[] { phoneNumber }, null);
+		if (c.moveToFirst()) {
+			threadId = c.getLong(0);
+		}
+		c.close();
+		if (threadId == -1) {
+			// Se non esiste, lo crea.
+			ContentValues values = new ContentValues();
+			values.put("phone_number", phoneNumber);
+			values.put("unread", 0);
+			values.put("last_update", now);
+			Uri uri = r.insert(MessagesContentProvider.THREADS_URI, values);
+			threadId = Long.parseLong(uri.getLastPathSegment());
+		} else {
+			// Se esiste, lo aggiorna.
+			ContentValues values = new ContentValues();
+			values.put("last_update", now);
+			r.update(MessagesContentProvider.THREADS_URI, values, "phone_number = ?", new String[] { phoneNumber });
+		}
+		// Aggiunge il messaggio.
+		ContentValues values = new ContentValues();
+		values.put("thread", threadId);
+		values.put("direction", 0);
+		values.put("message", message);
+		values.put("ts_sent", now);
+		r.insert(MessagesContentProvider.MESSAGES_URI, values);
+	}
 
 }
